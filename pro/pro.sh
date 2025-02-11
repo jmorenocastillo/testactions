2133c439a256710cc48ac5a8070a863586a1b39f
Your Name <you@example.com>
2025-02-11 20:40:40 +0100

    rrr

--- pro/pro.sh
+++ pro/pro.sh
@@ -1,13 +1,2 @@
-name: Run Pro Script
-
-on: [push]
-
-jobs:
-    run-pro-script:
-        runs-on: ubuntu-latest
-        steps:
-        - name: Checkout repository
-            uses: actions/checkout@v2
-
-        - name: Run pro.sh script
-            run: bash /C:/actiosn/testactions/pro/pro.sh
+echo "hello pro"
+sleep 20
