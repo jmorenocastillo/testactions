5a0c4be4640ecabb86cb81ef75bd47c8cb1891ce
Your Name <you@example.com>
2025-02-11 19:47:02 +0100

    init

--- pro/pro.sh
+++ pro/pro.sh
@@ -1,2 +1,13 @@
-echo "hello pro"
-sleep 20
+name: Run Pro Script
+
+on: [push]
+
+jobs:
+    run-pro-script:
+        runs-on: ubuntu-latest
+        steps:
+        - name: Checkout repository
+            uses: actions/checkout@v2
+
+        - name: Run pro.sh script
+            run: bash /C:/actiosn/testactions/pro/pro.sh
