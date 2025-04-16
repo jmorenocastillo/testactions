115042094acdd338eb65f2fb935c7660492f91a8
Your Name <you@example.com>
2025-04-16 08:51:08 +0200

    ff

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-          if [ -f .github/workflows/dynamic-deploy.yml ]; then
-            rm .github/workflows/dynamic-deploy.yml
-            echo "dynamic-deploy.yml deleted"
-          else
-            echo "dynamic-deploy.yml does not exist, nothing to delete"
-          fi
